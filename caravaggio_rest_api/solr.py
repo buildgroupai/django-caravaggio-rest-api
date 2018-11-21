b5eb60809c54e1b1fca55e5fb14639a79e83db39
xalperte <javier.alperte@gmail.com>
2018-11-21 01:32:12 +0100

    Added a class to help us query directly to Solr and paginate the results using a Cursor

--- caravaggio_rest_api/solr.py
+++ caravaggio_rest_api/solr.py
@@ -7,6 +7,7 @@ import json
 
 from django.core.exceptions import ImproperlyConfigured
 
+from haystack import connection_router, connections
 from haystack.backends import BaseEngine, EmptyResults
 from haystack.backends.solr_backend import SolrSearchQuery, SolrSearchBackend
 from haystack.constants import DJANGO_CT, DJANGO_ID, DEFAULT_ALIAS
@@ -102,6 +103,9 @@ class CassandraSolrSearchBackend(SolrSearchBackend):
                     results["facets"]["dates"][field_name] = \
                         group_facet_counts(range_data["counts"], 2)
 
+        if hasattr(raw_results, 'nextCursorMark'):
+            results["nextCursorMark"] = raw_results.nextCursorMark
+
         return results
 
     def build_search_kwargs(self, query_string, sort_by=None,
@@ -123,7 +127,7 @@ class CassandraSolrSearchBackend(SolrSearchBackend):
             models=models,
             limit_to_registered_models=limit_to_registered_models,
             result_class=result_class, stats=stats, collate=collate,
-            extra_kwargs=extra_kwargs)
+            **extra_kwargs)
 
         json_facets = extra_kwargs.get("json_facets", None)
         if json_facets:
@@ -340,3 +344,145 @@ class CassandraSolrSearchQuery(SolrSearchQuery):
 class CassandraSolrEngine(BaseEngine):
     backend = CassandraSolrSearchBackend
     query = CassandraSolrSearchQuery
+
+
+class CaravaggioSearchPaginator(object):
+
+    CURSORMARK_FIELD = 'cursorMark'
+    NEXT_CURSORMARK_FIELD = 'nextCursorMark'
+
+    def __init__(self, query_string, models=None, limit=None, using=None,
+                 max_limit=200, **kwargs):
+
+        self.using = using
+        self.query_string = query_string
+        self.limit = limit
+        self.cursorMark = "*"
+        self.max_limit = max_limit
+        self.search_kwargs = kwargs.copy()
+        if models:
+            self.search_kwargs["models"] = models
+        self.results = None
+        self.loaded_docs = 0
+        self.select_fields = None
+
+        self._determine_backend()
+
+    def reset(self):
+        self.cursorMark = "*"
+        self.results = None
+        self.loaded_docs = 0
+
+    def models(self, *models):
+        if not models:
+            if "models" in self.search_kwargs:
+                del self.search_kwargs["models"]
+        else:
+            self.search_kwargs["models"] = models
+
+        self._determine_backend()
+        return self
+
+    def select(self, *fields):
+        self.select_fields = fields
+        if not self.select_fields:
+            if "fl" in self.search_kwargs:
+                del self.search_kwargs["fl"]
+        else:
+            if "score" not in fields:
+                fields = list(fields)
+                fields.append("score")
+            self.search_kwargs["fl"] = fields
+        return self
+
+    def _determine_backend(self):
+        # A backend has been manually selected. Use it instead.
+        if self.using is not None:
+            self.backend = connections[self.using].get_backend()
+            return
+
+        # No backend, so rely on the routers to figure out what's right.
+        hints = {'models': self.models}
+
+        backend_alias = connection_router.for_read(**hints)
+
+        self.backend = connections[backend_alias].get_backend()
+
+    def get_hits(self):
+        return self.results["hits"] if self.results is not None else None
+
+    def get_results(self):
+        return self.results["results"] if self.results is not None else None
+
+    def get_raw_results(self):
+        return self.results
+
+    def get_loaded_docs(self):
+        return self.loaded_docs
+
+    def has_next(self):
+
+        # We cannot use CursorMarkets with Grouping. We will look if the
+        # number of results is less than the informed limit
+        is_group = 'group' in self.search_kwargs and \
+                   'true' == self.search_kwargs[str('group')]
+
+        if is_group:
+            valid_limit = self.limit \
+                if self.limit is not None and self.limit < self.max_limit \
+                else self.max_limit
+            return self.results is None or \
+                (0 < len(self.results['groups']) <= valid_limit)
+        else:
+            return self.results is None or self.cursorMark != \
+                   self.results[
+                       CaravaggioSearchPaginator.NEXT_CURSORMARK_FIELD]
+
+    def next(self):
+
+        if self.has_next():
+
+            # We cannot use CursorMarkets with Grouping. We will look if the
+            # number of results is less than the informed limit
+            is_group = 'group' in self.search_kwargs and \
+                       'true' == self.search_kwargs[str('group')]
+
+            if is_group:
+                self.search_kwargs[str('start_offset')] = self.loaded_docs
+
+                self.search_kwargs['end_offset'] = self.loaded_docs + (
+                    self.limit if self.limit is not None and
+                    self.limit < self.max_limit
+                    else self.max_limit)
+            else:
+
+                # Save the next cursor mark as the actual cursor mark to send
+                # to the server
+                if self.results:
+                    self.cursorMark = self.results[
+                        CaravaggioSearchPaginator.NEXT_CURSORMARK_FIELD]
+
+                # Signaling the cursor mark
+                if 'start_offset' in self.search_kwargs:
+                    del self.search_kwargs[str('start_offset')]
+
+                self.search_kwargs['end_offset'] = self.limit \
+                    if self.limit is not None and \
+                    self.limit < self.max_limit else self.max_limit
+
+                self.search_kwargs[
+                    CaravaggioSearchPaginator.CURSORMARK_FIELD] = \
+                    self.cursorMark
+
+            # Do the search
+            self.results = self.backend.search(
+                query_string=self.query_string, **self.search_kwargs)
+
+            if is_group:
+                self.loaded_docs += len(self.results['groups'])
+            else:
+                self.loaded_docs += len(self.results['results'])
+
+            return self.results
+        else:
+            return EmptyResults()
