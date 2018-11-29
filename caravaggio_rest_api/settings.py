ab55b0940e7b1609a99a923ca4074f79e349fffc
xalperte <javier.alperte@gmail.com>
2018-11-29 18:42:20 +0100

    Using our implementation of HaystackOrderingFilter to change the names of the fields when necessary to its form {}_exact (using the docValues version in the Solr index)

--- caravaggio_rest_api/settings.py
+++ caravaggio_rest_api/settings.py
@@ -336,7 +336,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS':
         ('drf_haystack.filters.HaystackFilter',
          'drf_haystack.filters.HaystackBoostFilter',
-         'drf_haystack.filters.HaystackOrderingFilter',),
+         'caravaggio_rest_api.drf_haystack.filters.HaystackOrderingFilter',),
 
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 
