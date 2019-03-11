936ee92c0a62e4e233c53a839aded60a62536867
xalperte <javier.alperte@gmail.com>
2019-03-11 17:10:10 +0100

    We need to fix the GDAL python library version to avoid compilation problems when installing the dependencies

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         'rest-framework-cache>=0.1',
         'django-redis>=4.10.0',
         'markdown>=2.6.11',
-        'gdal>=2.3.2',
+        'gdal==2.3.2',
         'geopy>=1.17.0',
         'drf-haystack>=1.8.4',
         'pysolr>=3.7.0',
