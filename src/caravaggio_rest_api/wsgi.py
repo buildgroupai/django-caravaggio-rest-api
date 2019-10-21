f25ed4918fb4e5490ac94ab6cc554a8d1b1e0b21
xalperte <javier.alperte@gmail.com>
2019-10-20 20:07:31 -0500

    Make use of the new Configuration system

--- src/caravaggio_rest_api/wsgi.py
+++ src/caravaggio_rest_api/wsgi.py
@@ -6,8 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "caravaggio_rest_api.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caravaggio_rest_api.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'development')
 
 application = get_wsgi_application()
