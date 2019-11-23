359e1ddfa61a5c146a35bbd50246843df20c6fd0
xalperte <javier.alperte@gmail.com>
2019-11-23 15:46:21 +0100

    Example of use of CaravaggioRouter to export only the  operation for the Search viewsets

--- src/caravaggio_rest_api/example/company/urls.py
+++ src/caravaggio_rest_api/example/company/urls.py
@@ -14,26 +14,36 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 
 """
+from django.conf import settings
 from django.conf.urls import url, include
 
 from caravaggio_rest_api.example.company.api.views import \
     CompanyViewSet, CompanySearchViewSet, CompanyGEOSearchViewSet
-from rest_framework import routers
+from caravaggio_rest_api.drf.routers import CaravaggioRouter
 
 # API v1 Router. Provide an easy way of automatically determining the URL conf.
 
-api_COMPANY = routers.DefaultRouter()
+api_SEARCH_COMPANY = CaravaggioRouter(actions=["list"])
 
-api_COMPANY.register(r'company/search',
-                     CompanySearchViewSet, base_name="company-search")
+if settings.DSE_SUPPORT:
+    api_SEARCH_COMPANY.register(
+        r'company/search',
+        CompanySearchViewSet,
+        base_name="company-search")
 
-api_COMPANY.register(r'company/geosearch',
-                     CompanyGEOSearchViewSet, base_name="company-geosearch")
+    api_SEARCH_COMPANY.register(
+        r'company/geosearch',
+        CompanyGEOSearchViewSet,
+        base_name="company-geosearch")
+
+api_COMPANY = CaravaggioRouter()
 
 api_COMPANY.register(r'company',
                      CompanyViewSet, base_name="company")
 
 urlpatterns = [
     # Company API version
-    url(r'^', include(api_COMPANY.urls), name="company-api"),
+    url(r'^',
+        include(api_SEARCH_COMPANY.urls + api_COMPANY.urls),
+        name="company-api"),
 ]
