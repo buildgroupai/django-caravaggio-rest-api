f011b909b725edf97dfc0a90a690263ee8e7c8de
xalperte <javier.alperte@gmail.com>
2020-05-08 13:09:14 +0200

    Checking if there is query_params. If there is not, we use the request.GET instead. In api unittest there is no query_params attribute available

--- src/caravaggio_rest_api/drf/authentication.py
+++ src/caravaggio_rest_api/drf/authentication.py
@@ -2,6 +2,9 @@
 # Copyright (c) 2019 BuildGroup Data Services Inc.
 # All rights reserved.
 import logging
+
+from django.contrib.auth.models import AnonymousUser
+
 from caravaggio_rest_api.users.models import CaravaggioOrganization, CaravaggioUser
 from django.conf import settings
 from django.utils.functional import SimpleLazyObject
@@ -11,11 +14,12 @@ _logger = logging.getLogger("caravaggio_rest_api.drf.authentication")
 
 
 def get_organization(request):
-    if request.user.is_superuser:
+    if request.user.is_superuser or isinstance(request.user, AnonymousUser):
         return None
 
-    if "_org_id" in request.query_params:
-        _org_id = request.query_params["_org_id"]
+    query_params = request.query_params if hasattr(request, "query_params") else request.GET
+    if "_org_id" in query_params:
+        _org_id = query_params["_org_id"]
         try:
             organization = CaravaggioOrganization.objects.get(id=_org_id)
             if organization in request.user.organizations.all():
@@ -41,11 +45,12 @@ class TokenAuthSupportQueryString(TokenAuthentication):
     def authenticate(self, request):
         # Check if 'token_auth' is in the request query params.
         # Give precedence to 'Authorization' header.
-        if settings.REST_FRAMEWORK["QUERY_STRING_AUTH_TOKEN"] in request.query_params and (
+        query_params = request.query_params if hasattr(request, "query_params") else request.GET
+        if settings.REST_FRAMEWORK["QUERY_STRING_AUTH_TOKEN"] in query_params and (
             "HTTP_AUTHORIZATION" not in request.META
         ):
             user_auth_tuple = self.authenticate_credentials(
-                request.query_params.get(settings.REST_FRAMEWORK["QUERY_STRING_AUTH_TOKEN"])
+                query_params.get(settings.REST_FRAMEWORK["QUERY_STRING_AUTH_TOKEN"])
             )
         else:
             user_auth_tuple = super(TokenAuthSupportQueryString, self).authenticate(request)
